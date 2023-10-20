c5fb195da19150e6e5b6a671fe32c200bb180393
bert <b.a.helderman@gmail.com>
2023-10-20 19:53:53 +0200

    commented out is bad, djust delete

--- slideshow/src/main/java/nl/ou/services/impl/SimpleSlideShowFactory.java
+++ slideshow/src/main/java/nl/ou/services/impl/SimpleSlideShowFactory.java
@@ -26,13 +26,6 @@ public class SimpleSlideShowFactory extends AbstractSlideShowFactory {
     @Override
     public SlideShow createSlideShow(SlideshowMeta meta, List<Slide> slides, List<Sequence> sequences) {
         List<Slide> allSlides = new ArrayList<>();
-        // if (addTitleSlide) {
-        //     allSlides.add(new TitleSlide(meta));
-        // }
-        // if (addTOCSlide) {
-        //     List<SlideMeta> slideMetaList = slides.stream().map(Slide::getMeta).collect(Collectors.toList());
-        //     allSlides.add(new TocSlide(slideMetaList));
-        // }
         allSlides.addAll(slides);
         return new SlideShowImpl(allSlides, sequences, meta);
     }
